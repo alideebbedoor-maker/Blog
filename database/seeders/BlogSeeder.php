201c0ffdcee56d7d19cb3b910ac4da0406ec01c2
Bedoor <alideebbedoor@gmail.com>
2025-12-13 00:41:13 +0300

    Add BlogSeeder+testimages+update seeder

--- database/seeders/BlogSeeder.php
+++ database/seeders/BlogSeeder.php
@@ -4,51 +4,45 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Blog;
-use App\Models\Category;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\File;
 
 class BlogSeeder extends Seeder
 {
     public function run()
     {
-        // Blogs demo data
+        
         $blogs = [
             [
-                'title' => 'Introduction to Laravel Framework',
-                'content' => 'Laravel is a powerful PHP framework used to build modern web applications...',
-                'image' => 'blogs/sample1.jpg'
+                'title' => 'Introduction to Laravel',
+                'content' => 'Laravel is a PHP framework for building web applications...',
+                'category_id' => 1,
+                'image' => 'sample1.jpg',
             ],
+        
+    
             [
-                'title' => 'Top 10 Travel Destinations in 2025',
-                'content' => 'If you love exploring new cities, here are the top places to visit in 2025...',
-                'image' => 'blogs/sample2.jpg'
+                'title' => 'Productivity Tips for Developers',
+                'content' => 'Increase your productivity with these tips...',
+                'category_id' => 1,
+                'image' => 'sample1.jpg',
             ],
-            [
-                'title' => 'Healthy Food Recipes for Beginners',
-                'content' => 'Here are some healthy food recipes you can try at home...',
-                'image' => 'blogs/sample3.jpg'
-            ],
-            [
-                'title' => 'How to Stay Motivated as a Developer',
-                'content' => 'Motivation is essential for growth in software development...',
-                'image' => 'blogs/sample4.jpg'
-            ],
-            [
-                'title' => 'Best Productivity Tips for Students',
-                'content' => 'Here are some effective tips to boost your productivity...',
-                'image' => 'blogs/sample5.jpg'
-            ],
+           
         ];
 
-        foreach ($blogs as $index => $data) {
+        foreach ($blogs as $data) {
+            $path = Storage::disk('public')->putFile(
+                'blogs',
+                new File(database_path('seeders/test_images/'.$data['image']))
+            );
+
             $blog = Blog::create([
                 'title' => $data['title'],
                 'content' => $data['content'],
-                'image' => $data['image']
+                'image' => $path,
             ]);
 
-            // attach category automatically
-            $categoryId = ($index % 5) + 1; // assigns categories 1 → 5
-            $blog->categories()->attach($categoryId);
+            $blog->categories()->attach($data['category_id']);
         }
     }
 }
